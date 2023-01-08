38eb5eddb74ad82fe9ca2dac7ad0868cbffb800d
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-01-08 17:25:15 -0300

    feat: Render pages dynamically

---
+++ pages/[...slug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { slug } = useRoute().params
+
+const story = await useAsyncStoryblok(
+  slug && Array.isArray(slug) ? slug.join('/') : 'home',
+  { version: 'draft' }
+)
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-const story = await useAsyncStoryblok('home', { version: 'draft' })
-</script>
-
-<template>
-  <StoryblokComponent v-if="story" :blok="story.content" />
-</template>
