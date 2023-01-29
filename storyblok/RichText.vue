3cf283ebf5cfff843c6b7fa6a692cb968398fa8c
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-01-28 22:53:05 -0300

    feat: Create RichText Storyblok component

---
+++ storyblok/RichText.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Richtext from 'storyblok-js-client'
+
+const props = defineProps<{
+  blok: {
+    id: string
+    content: Richtext
+  }
+}>()
+
+const content = computed(() => renderRichText(props.blok.content))
+</script>
+
+<template>
+  <div class="rich-text" v-editable="blok" :id="blok.id">
+    <div class="hero__rich-text" v-html="content" />
+  </div>
+</template>
