a075b4a30d61a556a0f228239df64c76abce5193
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-01-28 22:52:41 -0300

    feat: Create Skills and Skill Storyblok components

---
+++ storyblok/Skill.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ITechIcon } from '@/components/TechIcon.vue'
+
+defineProps<{
+  blok: {
+    label: string
+    icon: ITechIcon
+  }
+}>()
+</script>
+
+<template>
+  <div class="skill" v-editable="blok">
+    <TechIcon :icon="blok.icon" />
+    <span>{{ blok.label }}</span>
+  </div>
+</template>
---
+++ storyblok/Skills.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  blok: {
+    title: string
+    skills: object
+  }
+}>()
+</script>
+
+<template>
+  <div class="skills" v-editable="blok">
+    <h3>{{ blok.title }}</h3>
+    <StoryblokComponent
+      v-for="blok in blok.skills"
+      :key="blok._uid"
+      :blok="blok"
+    />
+  </div>
+</template>
