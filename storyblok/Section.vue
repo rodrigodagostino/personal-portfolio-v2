bca18d07b41266f562191550524e95a6e8ad98a9
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-01-28 22:50:52 -0300

    feat: Create Section Storyblok component

---
+++ storyblok/Section.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  blok: {
+    id: string
+    title: string
+    content: object
+  }
+}>()
+</script>
+
+<template>
+  <section class="section" v-editable="blok" :id="blok.id">
+    <h2 class="section__title">{{ blok.title }}</h2>
+    <StoryblokComponent
+      v-for="blok in blok.content"
+      :key="blok._uid"
+      :blok="blok"
+    />
+  </section>
+</template>
