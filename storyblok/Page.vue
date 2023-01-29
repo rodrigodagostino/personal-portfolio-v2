de27491af47304ee9ab9598ffae08e93e52658e3
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-01-28 22:54:06 -0300

    refactor: Add Header and Footer to Page

--- storyblok/Page.vue
+++ storyblok/Page.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
-defineProps<{ blok: Object }>()
+defineProps<{
+  blok: {
+    header: object
+    body: object
+    footer: object
+  }
+}>()
 </script>
 
 <template>
-  <div v-editable="blok">
-    <StoryblokComponent
-      v-for="blok in blok.body"
-      :key="blok._uid"
-      :blok="blok"
-    />
+  <div class="page" v-editable="blok">
+    <header id="page-header" class="page__header">
+      <StoryblokComponent
+        v-for="blok in blok.header"
+        :key="blok._uid"
+        :blok="blok"
+      />
+    </header>
+    <main id="page-main" class="page__main">
+      <StoryblokComponent
+        v-for="blok in blok.body"
+        :key="blok._uid"
+        :blok="blok"
+      />
+    </main>
+    <footer id="page-footer" class="page__footer">
+      <StoryblokComponent
+        v-for="blok in blok.footer"
+        :key="blok._uid"
+        :blok="blok"
+      />
+    </footer>
   </div>
 </template>
